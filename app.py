95cb6cc47d28c2b236922e474d1544665af5f5f8
nmoldakhmet <114348435+nmoldakhmet@users.noreply.github.com>
2025-05-23 14:53:58 +0500

    Update app.py
    small spelling mistake

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import glob
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_spliters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
